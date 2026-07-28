2d96ffcaa4f129cf2a4e1896f522e16d41108647
Miguel Layos.JS <miguelpogilayos@gmail.com>
2026-07-29 01:20:49 +0800

    Add scryfall and card routes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScryfallService } from './scryfall/scryfall.service';
+import { CardsService } from './cards/cards.service';
+import { CardsModule } from './cards/cards.module';
 
 @Module({
-  imports: [],
+  imports: [CardsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ScryfallService, CardsService],
 })
 export class AppModule {}
